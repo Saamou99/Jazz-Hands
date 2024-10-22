9d1f560a1db7911490bfc84c116f7ab2e03e6ba6
Saamou <90028820+Saamou99@users.noreply.github.com>
2024-10-23 01:39:22 +0200

    Part 3 Part 2

---
+++ Jazz Hands/Assets/Scenes/Scripts/ShooterScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ShooterScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPosition;
+
+    private float bulletSpeed = 10;
+
+    void Start()
+    {
+        XRGrabInteractable theGunGrabable = GetComponent<XRGrabInteractable>();
+        theGunGrabable.activated.AddListener(bangBang);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void bangBang (ActivateEventArgs arg)
+    {
+        GameObject newBullet = Instantiate(bullet);
+        newBullet.transform.position = spawnPosition.position;
+        newBullet.GetComponent<Rigidbody>().velocity = spawnPosition.forward * bulletSpeed;
+
+        Destroy(newBullet, 10);
+    }
+}
